f96b4fde6cde0ad947007eb8bab6248079d1c137
Anatolii Kabanov <4akabanov@gmail.com>
2020-06-25 16:07:07 +0200

    UI: Hello intro panel styles.

--- ya-ryadom-ui/src/components/panels/intro/hello-intro.panel.tsx
+++ ya-ryadom-ui/src/components/panels/intro/hello-intro.panel.tsx
@@ -1,3 +1,4 @@
+import './hello-intro.panel.scss';
 import React from 'react';
 import {
     Panel,
@@ -47,24 +48,26 @@ class HelloIntroPanel extends React.Component<AllProps>  {
     }
 
     render() {
-        const { id } = this.props;
+        const { id, vkUserInfo } = this.props;
         return (
-            <Panel id={id}>
+            <Panel id={id} className="hello-intro-panel">
                 <PanelHeader>
-                    Приветствуем!
+                    {vkUserInfo?.first_name && `Здравствуй, ${vkUserInfo.first_name}!`}
                 </PanelHeader>
-                <Group>
-                    <Div style={{ textAlign: 'center' }}>
+                <Group className="intro-group">
+                    <Div className="intro-image">
                         <IntroImage></IntroImage>
                     </Div>
-                    <Div>
-                        <p style={{ textAlign: 'center', fontWeight: 700, fontSize: 22 }}>Твой будущий друг уже рядом. <br /> Несколько действий и ты в большой тусовке своего города.</p>
-                    </Div>
-                    <Div>
-                        <p style={{ textAlign: 'center', fontWeight: 300 }}>Весь спектр интересов в одном приложении. <br /> Каждый найдёт себе дело по душе.</p>
-                    </Div>
-                    <Div>
-                        <Button size="l" className="btn-primary" onClick={() => this.onIntroCompleted()}>Начать</Button>
+                    <Div className="intro-text">
+                        <Div>
+                            <p className="first-row-text">Твой будущий друг уже рядом. <br /> Несколько действий и ты в большой тусовке своего города.</p>
+                        </Div>
+                        <Div>
+                            <p className="second-row-text">Весь спектр интересов в одном приложении. <br /> Каждый найдёт себе дело по душе.</p>
+                        </Div>
+                        <Div>
+                            <Button size="l" className="btn-primary" onClick={() => this.onIntroCompleted()}>Начать</Button>
+                        </Div>
                     </Div>
                 </Group>
             </Panel>
