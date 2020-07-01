abea340237a5e133e5d19788b6a6848b622bbb8c
Anatolii Kabanov <4akabanov@gmail.com>
2020-07-01 16:39:32 +0200

    UI: Application sagas.

--- ya-ryadom-ui/src/store/applications/sagas.ts
+++ ya-ryadom-ui/src/store/applications/sagas.ts
@@ -1,6 +1,6 @@
 import { all, call, fork, put, takeEvery, takeLatest, select } from 'redux-saga/effects';
 import { ApplicationsTypes } from './types';
-import { fetchEventApplicantsRequest, fetchEventApplicantsError, fetchEventApplicantsSuccess, applyToEventRequest, applyToEventError, applyToEventSuccess, confirmApplicantRequest, confirmApplicantError, confirmApplicantSuccess } from './actions'
+import { fetchEventApplicantsRequest, fetchEventApplicantsError, fetchEventApplicantsSuccess, applyToEventRequest, applyToEventError, applyToEventSuccess, confirmApplicantRequest, confirmApplicantError, confirmApplicantSuccess, rejectApplicantRequest, rejectApplicantError, rejectApplicantSuccess, revokeApplicationRequest, revokeApplicationError, revokeApplicationSuccess } from './actions'
 import { callApi } from '../../utils/api';
 import { Application } from './models';
 import { getVkUserId } from '../authentication/reducer';
@@ -64,12 +64,12 @@ function* handleConfirmApplicantRequest(action: ReturnType<typeof confirmApplica
     try {
         const vkUserId = yield select(getVkUserId);
 
-        const confirmApplicant: Application = {
+        const application: Application = {
             vkUserId: vkUserId,
             eventId: action.payload,
         };
 
-        const result = yield call(callApi, 'post', API_ENDPOINT, '/confirm', confirmApplicant);
+        const result = yield call(callApi, 'post', API_ENDPOINT, '/confirm', application);
 
         if (result.errors) {
             yield put(confirmApplicantError(result.errors));
@@ -91,11 +91,75 @@ function* watchConfirmApplicantRequest() {
     yield takeLatest(ApplicationsTypes.CONFIRM_APPLICANT, handleConfirmApplicantRequest)
 }
 
+function* handleRejectApplicantRequest(action: ReturnType<typeof rejectApplicantRequest>) {
+    try {
+        const vkUserId = yield select(getVkUserId);
+
+        const application: Application = {
+            vkUserId: vkUserId,
+            eventId: action.payload,
+        };
+
+        const result = yield call(callApi, 'post', API_ENDPOINT, '/reject', application);
+
+        if (result.errors) {
+            yield put(rejectApplicantError(result.errors));
+        } else {
+            yield put(rejectApplicantSuccess(action.payload));
+        }
+    } catch (error) {
+        if (error instanceof Error && error.stack) {
+            yield put(rejectApplicantError(error.stack));
+        } else {
+            yield put(rejectApplicantError('An unknown error occured.'));
+        }
+    } finally {
+
+    }
+}
+
+function* watchRejectApplicantRequest() {
+    yield takeLatest(ApplicationsTypes.REJECT_APPLICANT, handleRejectApplicantRequest)
+}
+
+function* handleRevokeApplicationRequest(action: ReturnType<typeof revokeApplicationRequest>) {
+    try {
+        const vkUserId = yield select(getVkUserId);
+
+        const application: Application = {
+            vkUserId: vkUserId,
+            eventId: action.payload,
+        };
+
+        const result = yield call(callApi, 'post', API_ENDPOINT, '/revoke', application);
+
+        if (result.errors) {
+            yield put(revokeApplicationError(result.errors));
+        } else {
+            yield put(revokeApplicationSuccess(action.payload));
+        }
+    } catch (error) {
+        if (error instanceof Error && error.stack) {
+            yield put(revokeApplicationError(error.stack));
+        } else {
+            yield put(revokeApplicationError('An unknown error occured.'));
+        }
+    } finally {
+
+    }
+}
+
+function* watchRevokeApplicationRequest() {
+    yield takeLatest(ApplicationsTypes.REVOKE_APPLICATION, handleRevokeApplicationRequest)
+}
+
 function* applicationsSagas() {
     yield all([
         fork(watchFetchEventApplicantsRequest),
         fork(watchApplyToEventRequest),
-        fork(watchConfirmApplicantRequest)
+        fork(watchConfirmApplicantRequest),
+        fork(watchRejectApplicantRequest),
+        fork(watchRevokeApplicationRequest)
     ])
 }
 
