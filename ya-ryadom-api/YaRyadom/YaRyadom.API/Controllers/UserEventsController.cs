779283b8bfe3ede26caa9f5a3a7937d94e5b8088
Anatolii Kabanov <4akabanov@gmail.com>
2020-07-20 17:44:41 +0200

    API: User events in separate controller.

---
+++ ya-ryadom-api/YaRyadom/YaRyadom.API/Controllers/UserEventsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+using YaRyadom.API.Services.Interfaces;
+
+namespace YaRyadom.API.Controllers
+{
+	public class UserEventsController : ControllerBase
+	{
+		private readonly IUserEventsService _userEventsService;
+
+		public UserEventsController(IUserEventsService userEventsService)
+		{
+			_userEventsService = userEventsService ?? throw new ArgumentNullException(nameof(userEventsService));
+		}
+
+		[AllowAnonymous]
+		[HttpGet("created/{vkUserId}")]
+		[Consumes(MediaTypeNames.Application.Json)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public async Task<IActionResult> GetCreatedEvents(long vkUserId, CancellationToken cancellationToken = default)
+		{
+			var result = await _userEventsService.GetCreatedEvents(vkUserId, cancellationToken).ConfigureAwait(false);
+			return Ok(result);
+		}
+
+		[AllowAnonymous]
+		[HttpGet("visited/{vkUserId}")]
+		[Consumes(MediaTypeNames.Application.Json)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public async Task<IActionResult> GetVisitedEvents(long vkUserId, CancellationToken cancellationToken = default)
+		{
+			var result = await _userEventsService.GetVisitedEvents(vkUserId, cancellationToken).ConfigureAwait(false);
+			return Ok(result);
+		}
+	}
+}
---
+++ ya-ryadom-api/YaRyadom/YaRyadom.API/Services/Interfaces/IUserEventsService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using YaRyadom.API.Models;
+using YaRyadom.Domain.Entities;
+
+namespace YaRyadom.API.Services.Interfaces
+{
+	public interface IUserEventsService: IBaseService<YaRyadomEvent>
+	{
+		Task<UserEventModel[]> GetCreatedEvents(long vkId, CancellationToken cancellationToken = default);
+
+		Task<UserEventModel[]> GetVisitedEvents(long vkId, CancellationToken cancellationToken = default);
+	}
+}
