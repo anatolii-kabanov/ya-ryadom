74b7b16a1b86c0530b67ff15c15803aa9420a691
Anatolii Kabanov <4akabanov@gmail.com>
2020-10-30 22:46:59 +0100

    UI: Complaint already sent logic.

--- ya-ryadom-ui/src/store/complaints/sagas.ts
+++ ya-ryadom-ui/src/store/complaints/sagas.ts
@@ -1,17 +1,17 @@
-import { all, call, fork, put, takeEvery, takeLatest, select, take } from 'redux-saga/effects';
+import { all, call, fork, put, takeLatest, select } from 'redux-saga/effects';
 import { ComplaintsTypes } from './types';
 import {
-    sendComplaintToEventRequest,
-    sendComplaintToEventSuccess,
-    sendComplaintToEventError,
-    sendComplaintToUserRequest,
-    sendComplaintToUserError,
-    sendComplaintToUserSuccess,
-    openEventComplaintForm,
-    setSelectedEventId
-} from './actions'
+	sendComplaintToEventRequest,
+	sendComplaintToEventSuccess,
+	sendComplaintToEventError,
+	sendComplaintToUserRequest,
+	sendComplaintToUserError,
+	sendComplaintToUserSuccess,
+	openEventComplaintForm,
+	setSelectedEventId,
+} from './actions';
 import { callApi } from '../../utils/api';
-import { ComplaintToEventRequest, ComplaintToUserRequest } from './models';
+import { ComplaintToEventRequest } from './models';
 import { getVkUserId } from '../authentication/reducer';
 import { showSpinner, hideSpinner } from '../ui/spinner/actions';
 import { setActiveModal } from '../history/actions';
@@ -23,88 +23,124 @@ import { NOTIFICATION_MESSAGES } from '../../utils/constants/notification-messag
 
 const API_ENDPOINT: any = `${process.env.REACT_APP_API_ENDPOINT}/complaints`;
 
-function* handleSendComplaintToEventRequest(action: ReturnType<typeof sendComplaintToEventRequest>) {
-    try {
-        yield put(showSpinner());
-        const vkUserId = yield select(getVkUserId);
-        const eventId = yield select(getSelectedEventId);
+function* handleSendComplaintToEventRequest(
+	action: ReturnType<typeof sendComplaintToEventRequest>,
+) {
+	try {
+		yield put(showSpinner());
+		const vkUserId = yield select(getVkUserId);
+		const eventId = yield select(getSelectedEventId);
 
-        const complaintToEvent: ComplaintToEventRequest = {
-            vkUserId: vkUserId,
-            eventId: eventId,
-            complaintType: action.payload.selectedComplaint,
-            text: action.payload.text
-        };
+		const complaintToEvent: ComplaintToEventRequest = {
+			vkUserId: vkUserId,
+			eventId: eventId,
+			complaintType: action.payload.selectedComplaint,
+			text: action.payload.text,
+		};
 
-        const result = yield call(callApi, 'post', API_ENDPOINT, '/to-event', complaintToEvent);
+		const result = yield call(
+			callApi,
+			'post',
+			API_ENDPOINT,
+			'/to-event',
+			complaintToEvent,
+		);
 
-        if (result.errors) {
-            yield put(sendComplaintToEventError(result.errors));
-        } else {
-            yield put(sendComplaintToEventSuccess());
-            yield put(setActiveModal(null));
-        }
-    } catch (error) {
-        yield put(addNotificaiton(new SnackbarErrorNotification(NOTIFICATION_MESSAGES.REQUEST_ERROR)));
-        if (error instanceof Error && error.stack) {
-            yield put(sendComplaintToEventError(error.stack));
-        } else {
-            yield put(sendComplaintToEventError('An unknown error occured.'));
-        }
-    } finally {
-        yield put(hideSpinner());
-    }
+		if (result.errors || result?.status >= 400) {
+			yield put(
+				addNotificaiton(
+					new SnackbarErrorNotification(NOTIFICATION_MESSAGES.COMPLAINT_ALREADY_SENT),
+				),
+			);
+			yield put(sendComplaintToEventError(result.errors));
+		} else {
+			yield put(sendComplaintToEventSuccess());
+			yield put(setActiveModal(null));
+		}
+	} catch (error) {
+		yield put(
+			addNotificaiton(
+				new SnackbarErrorNotification(NOTIFICATION_MESSAGES.REQUEST_ERROR),
+			),
+		);
+		if (error instanceof Error && error.stack) {
+			yield put(sendComplaintToEventError(error.stack));
+		} else {
+			yield put(sendComplaintToEventError('An unknown error occured.'));
+		}
+	} finally {
+		yield put(hideSpinner());
+	}
 }
 
 function* watchSendComplaintToEventRequest() {
-    yield takeLatest(ComplaintsTypes.SEND_COMPLAINT_TO_EVENT, handleSendComplaintToEventRequest)
+	yield takeLatest(
+		ComplaintsTypes.SEND_COMPLAINT_TO_EVENT,
+		handleSendComplaintToEventRequest,
+	);
 }
 
-function* handleSendComplaintToUserRequest(action: ReturnType<typeof sendComplaintToUserRequest>) {
-    try {
-        yield put(showSpinner());
-        const vkUserId = yield select(getVkUserId);
+function* handleSendComplaintToUserRequest(
+	action: ReturnType<typeof sendComplaintToUserRequest>,
+) {
+	try {
+		yield put(showSpinner());
+		const vkUserId = yield select(getVkUserId);
 
-        const applyToUser = action.payload;
-        applyToUser.vkUserId = vkUserId;
+		const applyToUser = action.payload;
+		applyToUser.vkUserId = vkUserId;
 
-        const result = yield call(callApi, 'post', API_ENDPOINT, '/to-user', applyToUser);
+		const result = yield call(
+			callApi,
+			'post',
+			API_ENDPOINT,
+			'/to-user',
+			applyToUser,
+		);
 
-        if (result.errors) {
-            yield put(sendComplaintToUserError(result.errors));
-        } else {
-            yield put(sendComplaintToUserSuccess());
-        }
-    } catch (error) {
-        if (error instanceof Error && error.stack) {
-            yield put(sendComplaintToUserError(error.stack));
-        } else {
-            yield put(sendComplaintToUserError('An unknown error occured.'));
-        }
-    } finally {
-        yield put(hideSpinner());
-    }
+		if (result.errors) {
+			yield put(sendComplaintToUserError(result.errors));
+		} else {
+			yield put(sendComplaintToUserSuccess());
+		}
+	} catch (error) {
+		if (error instanceof Error && error.stack) {
+			yield put(sendComplaintToUserError(error.stack));
+		} else {
+			yield put(sendComplaintToUserError('An unknown error occured.'));
+		}
+	} finally {
+		yield put(hideSpinner());
+	}
 }
 
 function* watchSendComplaintToUserRequest() {
-    yield takeLatest(ComplaintsTypes.SEND_COMPLAINT_TO_USER, handleSendComplaintToUserRequest)
+	yield takeLatest(
+		ComplaintsTypes.SEND_COMPLAINT_TO_USER,
+		handleSendComplaintToUserRequest,
+	);
 }
 
-function* handleOpenEventComplaintForm(action: ReturnType<typeof openEventComplaintForm>) {
-    yield put(setSelectedEventId(action.payload));
-    yield put(setActiveModal(MODALS.COMPLAINT));
+function* handleOpenEventComplaintForm(
+	action: ReturnType<typeof openEventComplaintForm>,
+) {
+	yield put(setSelectedEventId(action.payload));
+	yield put(setActiveModal(MODALS.COMPLAINT));
 }
 
 function* watchOpenEventComplaintForm() {
-    yield takeLatest(ComplaintsTypes.OPEN_EVENT_COMPLAINT_FORM, handleOpenEventComplaintForm)
+	yield takeLatest(
+		ComplaintsTypes.OPEN_EVENT_COMPLAINT_FORM,
+		handleOpenEventComplaintForm,
+	);
 }
 
 function* complaintsSagas() {
-    yield all([
-        fork(watchSendComplaintToEventRequest),
-        fork(watchSendComplaintToUserRequest),
-        fork(watchOpenEventComplaintForm)
-    ])
+	yield all([
+		fork(watchSendComplaintToEventRequest),
+		fork(watchSendComplaintToUserRequest),
+		fork(watchOpenEventComplaintForm),
+	]);
 }
 
 export default complaintsSagas;
--- ya-ryadom-ui/src/utils/constants/notification-messages.constants.ts
+++ ya-ryadom-ui/src/utils/constants/notification-messages.constants.ts
@@ -1,5 +1,6 @@
 export const NOTIFICATION_MESSAGES = {
     REQUEST_ERROR: 'Произошла ошибка. Попробуйте ещё раз.',
     COMPLAINT_SEND_SUCCESS: 'Жалоба отправлена. Спасибо.',
+    COMPLAINT_ALREADY_SENT: 'Вы уже отправили жалобы на данное событие. Спасибо.',
     CHECK_INTERNET_CONNECTION: 'Проверьте подключение к интернету.',
 };
